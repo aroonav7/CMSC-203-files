ef9e50191285bd9c2a34ec75b543c1fadbe3dd0b
aroonav7 <60497668+aroonav7@users.noreply.github.com>
2020-02-05 02:00:34 -0500

    First commit. Uploading all files.

---
+++ Wind Chill/src/WindChill.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class WindChill {
+
+	public static void main(String[] args) {
+		
+				Scanner input = new Scanner(System.in);
+
+				System.out.print("Enter the temperature in Fahrenheit " +
+					"between -45ºF and 40ºF: ");
+				double temperature = input.nextDouble();
+				System.out.print("Enter the wind speed between 5 and 60 miles per hour: ");
+				double speed = input.nextDouble();
+				double windChill = 35.74 + 0.6215 * temperature -
+										 35.75 * Math.pow(speed, 0.16) +
+										 0.4275 * temperature * Math.pow(speed, 0.16);
+
+				System.out.println("The wind chill index is " + windChill + " degrees Fahrenheit");
+				System.out.println("Programmer: Aroonav Pradhan");
+			}
+		
+
+	}
